8b5608cac5c3b8abb0a8a4b23755c75fa175d2b0
mah00j <mah06.hajj@gmail.com>
2025-09-18 23:50:18 +0300

    Implement optimized solution for Remove Duplicates from Sorted Array

--- LeetCode/RemoveDupFromSortedArr.java
+++ LeetCode/RemoveDupFromSortedArr.java
@@ -12,7 +12,7 @@ class Solution {
 	return i;
     }
 }
-
+//this solution uses extra space of HashSet.
 // 26. Remove Duplicates from Sorted Array
 // Hint
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
