205a65e41089dd3961ed8ea4a2e860a6efae28ba
Mahmoud-Al-Hajj <mah06.hajj@gmail.com>
2026-02-06 23:02:13 +0200

    Add more optimal solution

--- LeetCode/ProductofArrayExceptSelf.java
+++ LeetCode/ProductofArrayExceptSelf.java
@@ -1,22 +1,18 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int [] left_products = new int[n];
-        int [] right_products = new int[n];
         int [] output_arr = new int[n];
-
-        left_products[0]=1;
-        right_products[n-1]=1;
+        output_arr[0]=1;
 
         for(int i=1;i<n;i++){
-            left_products[i] =nums[i-1] * left_products[i-1];
+            output_arr[i] =nums[i-1] * output_arr[i-1];
         }
-        for(int i=n-2;i>=0;i--){
-            right_products[i]= nums[i+1] * right_products[i+1];
+        int R=1;
+        for(int i=n-1;i>=0;i--){
+            output_arr[i]= output_arr[i] * R;
+            R = R * nums[i];
         }
-        for(int i=0;i<n;i++){
-            output_arr[i] = left_products[i] * right_products[i];
-        }
+
     return output_arr;
     }
 }
